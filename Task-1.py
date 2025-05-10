54812a4e53aaffaae82c70e9c9caedd99b7f3d07
amannn409 <cuteaman03409@gmail.com>
2025-05-10 18:10:12 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,26 @@
+
+# Enter first number: 5
+# Enter second number: 10
+# Performing following operations:
+ 
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+
+# Additon:
+sum=num1+num2
+print("the sum of",sum)
+
+# Subtraction:
+min=num1-num2
+print("the subtraction of",min)
+
+# Multiplication:
+product=num1*num2
+print("the multipliction of",product)
+
+# Division
+divide=num1/num2
+print("the division of",divide)
+
+
---
+++ Task-2.py
@@ -0,0 +1,8 @@
+# Problem Statement:
+# Enter first name : John
+# Enter second name : Doe
+
+first_name=(input("enter first name:"))
+last_name=(input("enter second name:"))
+fullname= first_name + " " + last_name + "!"
+print("Hello,",fullname," welcome to the python world.")
